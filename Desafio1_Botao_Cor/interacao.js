1e1f2a607702e71b3fad85d9d3f02e15928446a9
Augustinho Baumgarten <106928471+AugustinhoBaumgarten@users.noreply.github.com>
2025-11-19 17:16:07 -0300

    botão + / -

---
+++ Desafio1_Botao_Cor/interacao.js
@@ -0,0 +1,4 @@
+function botao(){
+    var texto = document.getElementById('texto1')
+     texto.innerText = 'TEXTO 2'
+}
---
+++ Desafio2_Contador/interacao.js
@@ -0,0 +1,17 @@
+function mais(){
+    var caixa = document.getElementById('cxNumero')
+    caixa.value = Number(caixa.value) + 1
+
+}
+
+function menos(){
+    var caixa = document.getElementById('cxNumero')
+
+    if(caixa.value > 0){
+    caixa.value = Number(caixa.value) - 1
+    }
+}
+
+if(caixa.value > 5){
+    
+}
