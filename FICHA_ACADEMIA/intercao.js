b0402ea569775ad0910e63f4cb74fafe7a7fad82
Augustinho Baumgarten <106928471+AugustinhoBaumgarten@users.noreply.github.com>
2025-12-20 17:50:00 -0300

    site ficha treino
    index, interacao.js e design.css

---
+++ FICHA_ACADEMIA/intercao.js
@@ -0,0 +1,13 @@
+const adicionar = document.getElementById('adicionar');
+const texto = document.getElementById('texto');
+const exercicios = document.getElementById('exercicios')
+
+let listaEx = [];
+
+
+adicionar.onclick = () =>{
+    const valorTexto = texto.value.trim();
+    if(valorTexto === "") return;
+
+    valorTexto = "";
+}
